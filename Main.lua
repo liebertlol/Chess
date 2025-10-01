47d326f1e3ee59dedc84180a43c77e57d0a78e8f
liebertlol <gilates6@gmail.com>
2025-10-01 21:22:48 +0700

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,59 +1,55 @@
 local SE = setmetatable({}, {
     __index = function(self, key)
-        local ok, s = pcall(function()
-            return game:GetService(key)
-        end)
+        local ok, s = pcall(game.GetService, game, key)
         if ok and s then
             rawset(self, key, s)
             return s
         end
-        return nil
     end
 })
 
 if not isfolder("ChessCheat") then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/liebertlol/Chess/refs/heads/main/Installer.lua",true))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/liebertlol/Chess/refs/heads/main/Installer.lua"))()
 end
 
 local Players = SE.Players
+local lplr = Players.LocalPlayer
 local Workspace = SE.Workspace
 local RS = SE.ReplicatedStorage
-local RunService = SE.RunService
 local HttpService = SE.HttpService
 local fetch = (syn and syn.request) or (http and http.request) or http_request or (fluxus and fluxus.request) or request
+local parser = loadfile("ChessCheat/Library/getFEN")()
+local depth = 12 -- max 18
 
-local fenParser = loadfile("ChessCheat/Library/getFen.lua")()
-local utils = loadfile("ChessCheat/Library/Utils.lua")()
-local FEN = fenParser.getFEN()
-
-function Move(...)
-  RS.Chess.SumbitMove(...)
-  return true
-end -- useless 
-
-function GetMove(FEN, Engine)
-    if Engine == "Stockfish" then
-        local Res;
-        local suc, ret = pcall(function()
-            Res = fetch({
-                Url = "https://chess-api.com/v1",
-                Method = "POST",
-                Headers = { ["Content-Type"] = "application/json" },
-                Body = HttpService:JSONEncode({
-                    fen = FEN
-                })
-            })
-        end)
-    if suc and Res and Res.Success then
-      local Data = HttpService:JSONDecode(Res.Body)
-      return Data.from .. Data.to
-    else
-      warn("[Engine] Stockfish request failed:", ret or (Res and Res.StatusCode))
+function move(move)
+    if move and #move >= 4 then
+        RS.Chess.SubmitMove:InvokeServer(move)
     end
-  end
+end
+
+function getMove(fen)
+    local Res
+    local suc, ret = pcall(function()
+        Res = fetch({
+            Url = "https://chess-api.com/v1",
+            Method = "POST",
+            Headers = {["Content-Type"] = "application/json"},
+            Body = HttpService:JSONEncode({
+            fen = fen,
+            depth = depth or 12
+            })
+        })
+    end)
+    if suc and Res and Res.Success then
+        local Data = HttpService:JSONDecode(Res.Body)
+        return Data.from .. Data.to
+    end
 end
 
 while task.wait() do
-  local Moves = GetMove(FEN, "Stockfish")
-  Move(Moves)
+    local fen = parser.FEN()
+    if fen then
+        local moved = getMove(fen)
+        move(moves)
+    end
 end

46916aacef4dee3772fa4e6098a6d997f4567627
liebertlol <gilates6@gmail.com>
2025-10-01 20:19:01 +0700

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -17,7 +17,7 @@ end
 
 local Players = SE.Players
 local Workspace = SE.Workspace
-local ReplicatedStorage = SE.ReplicatedStorage
+local RS = SE.ReplicatedStorage
 local RunService = SE.RunService
 local HttpService = SE.HttpService
 local fetch = (syn and syn.request) or (http and http.request) or http_request or (fluxus and fluxus.request) or request
@@ -25,6 +25,11 @@ local fetch = (syn and syn.request) or (http and http.request) or http_request o
 local fenParser = loadfile("ChessCheat/Library/getFen.lua")()
 local utils = loadfile("ChessCheat/Library/Utils.lua")()
 local FEN = fenParser.getFEN()
+
+function Move(...)
+  RS.Chess.SumbitMove(...)
+  return true
+end -- useless 
 
 function GetMove(FEN, Engine)
     if Engine == "Stockfish" then
@@ -50,5 +55,5 @@ end
 
 while task.wait() do
   local Moves = GetMove(FEN, "Stockfish")
-  utils.Move(Moves)
+  Move(Moves)
 end
